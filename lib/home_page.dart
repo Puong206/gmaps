e6e668cb614f042ced8dee571810031e30de22b9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-08 10:15:29 +0700

    feat: tambahkan tombol untuk memilih alamat di halaman utama

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,7 +24,27 @@ class _HomePageState extends State<HomePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Row(
-                        
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Pilih Alamat:"),
+                          IconButton(
+                            icon: const Icon(Icons.map, color: Colors.blue),
+                            onPressed: () async {
+                              final result = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => 
+                                  const MapPage()
+                                )
+                              );
+                              if (result != null) {
+                                setState(() {
+                                  alamatDipilih = result;
+                                });
+                              }
+                            },
+                          )
+                        ],
                       )
                     ],
                   )
