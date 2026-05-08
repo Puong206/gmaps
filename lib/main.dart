89a52bcbea1990395e5f06be36ec8b101a76b3b3
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-08 11:30:27 +0700

    feat: tambahkan inisialisasi peta dan struktur aplikasi utama

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gmaps/home_page.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,3 +15,25 @@ void main() {
   runApp(const MyApp());
 }
 
+void initializeMapRenderer() {
+  final GoogleMapsFlutterPlatform mapsImplementation = GoogleMapsFlutterPlatform.instance;
+  if (mapsImplementation is GoogleMapsFlutterAndroid) {
+    await mapsImplementation.initializeWithRenderer(AndroidMapRenderer.latest);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
