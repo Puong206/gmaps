b3019fa3e93a2a35dbc586a6000c7b22dbd7dba8
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-08 10:04:24 +0700

    feat: tambahkan implementasi halaman utama dengan widget HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,3 +4,18 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
+  String? alamatDipilih;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Maps'),
+      ),
+      body: const Center(
+        child: Text('Google Maps will be displayed here.'),
+      ),
+    );
+  }
+}
